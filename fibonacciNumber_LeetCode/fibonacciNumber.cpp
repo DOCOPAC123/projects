bba1aca504af6ca7b31a617c40136e95055b4f59
Rohit Chauhan <rohit1.chauhan@ril.com>
2019-01-28 14:39:56 +0530

    fibonacciNumber

---
+++ fibonacciNumber_LeetCode/fibonacciNumber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int fib(int N) {
+        if(N==0)
+        {
+            return 0;
+        }
+        else
+        if(N==1)
+        {
+            return 1;
+        }
+        else
+        {
+            int a = 0;
+            int b = 1;
+            int c;
+            for(int i=2;i<=N;i++)
+            {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
+};
